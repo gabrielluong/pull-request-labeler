096f453716518cc411149c8b16e5a2968866ecc3
Gabriel Luong <gabriel.luong@gmail.com>
2023-05-15 16:49:06 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const github = require("@actions/github");
+const core = require('@actions/core');
+
+async function run() {
+  try {
+    const token = core.getInput("github-token");
+    const octokit = new github.getOctokit(token);
+    const payload = github.context.payload;
+    const repo = payload.repository.name;
+    const owner = payload.repository.owner.login;
+    const pullRequestNumber = payload.number;
+
+    const labels = new Set();
+
+    if (pullRequestNumber === undefined) {
+      core.warning("No pull request number in payload.");
+      return;
+    }
+
+    const { data: pullRequest } = await octokit.rest.pulls.get({
+      owner,
+      repo,
+      pull_number: pullRequestNumber,
+    });
+
+    if (pullRequest["author_association"].includes("CONTRIBUTOR")) {
+      labels.add("contributor");
+    }
+
+    if (pullRequest.draft) {
+      labels.add("work in progress");
+    } else {
+      const { data: reviews } = await octokit.rest.pulls.listReviews({
+        owner,
+        repo,
+        pull_number: pullRequestNumber,
+      });
+
+      for (const review of reviews) {
+        if (review["author_association"] == "MEMBER" || review["author_association"] == "OWNER") {
+          switch (review.state) {
+            case "APPROVED":
+              labels.add("approved");
+              break;
+            case "CHANGES_REQUESTED":
+              labels.add("changes required");
+          }
+        }
+      }
+
+      if (labels.has("changes required")) {
+        labels.delete("approved");
+      } else if (!labels.size) {
+        labels.add("🕵️‍♀️ needs review");
+      }
+    }
+
+    await octokit.rest.issues.setLabels({
+      owner,
+      repo,
+      issue_number: pullRequestNumber,
+      labels: Array.from(labels),
+    });
+
+    core.notice(`Added labels to #${pullRequestNumber}.`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
